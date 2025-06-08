2f246c50cca13b2b0a7a565724254239bd6e66fd
Sushank <sushankscras27@gmail.com>
2025-06-08 15:18:24 +0545

    Added Comment for to make easier to see for future use.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,14 @@
 <body>
 
     @auth
+        {{-- this is for knowing if you're logged in and for logout button after getting logged in --}}
         <p>You are logged in</p>
         <form action="/logout" method="POST">
             @csrf
             <button>Log out</button>
         </form>    
     @else
+        {{--This is to get the home page if error occurs--}}
         <div style="border: 3px solid black; padding: 1em; width: 300px;">
             <h2>Register</h2>
             <form action="/register" method="POST">
@@ -26,6 +28,7 @@
             </form>
         </div>
         
+        {{--To login with the existing Name and password--}}
         <div style="border: 3px solid black; padding: 1em; width: 300px;">
             <h2>Login</h2>
             <form action="/login" method="POST">
